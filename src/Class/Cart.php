48a5f74c6874a44a1de35f10a87a5b36b2465aa5
3abkerm <3abkerim@gmail.com>
2023-12-15 09:21:15 +0100

    Commit 14/12

---
+++ src/Class/Cart.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Class;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Cart
+{
+    private $stack;
+
+    public function __construct(RequestStack $stack)
+    {
+        $this->stack = $stack;
+    }
+
+    public function add($id)
+    {
+        $session = $this->stack->getSession();
+        $cart = $session->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+
+        $session->set('cart', $cart);
+    }
+
+    public function get()
+    {
+        $methodget = $this->stack->getSession();
+        return $methodget->get('cart');
+    }
+
+    public function remove()
+    {
+
+        $methodremove = $this->stack->getSession();
+        return $methodremove->remove('cart');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index()
     {
         return $this->render('home/index.html.twig');
     }
 }
+
+// <?php
+
+// namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\Session\SessionInterface;
+// use Symfony\Component\Routing\Annotation\Route;
+
+// class HomeController extends AbstractController
+// {
+//     #[Route('/', name: 'home')]
+//     public function index(SessionInterface $session): Response
+//     {
+//         $session->set('cart', [
+//             [
+//                 'id' => 522,
+//                 'quantity' => 12
+//             ]
+//         ]);
+
+//         $cart = $session->get('cart');
+//         dd($cart);
+//         return $this->render('home/index.html.twig');
+//     }
+// }
