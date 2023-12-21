d2c9197dbd13648d129551225dd24fb08546a3ec
Abdelkerim Abdelaziz <122649417+3abkerim@users.noreply.github.com>
2023-12-21 15:56:58 +0100

    21/12/2023

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Address;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,17 +16,65 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('address')
-            ->add('postal')
-            ->add('city')
-            ->add('country')
-            ->add('phone')
+            ->add('name', TextType::class, [
+                'label' => 'Quel nom souhaitez-vous donner à votre adresse ?',
+                'attr' => [
+                    'placeholder' => 'Nommez votre adresse'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Votre nom'
+                ]
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Société',
+                'attr' => [
+                    'placeholder' => 'Votre société (facultatif)'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Votre adresse'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'Votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Votre ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'Votre pays'
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Numéro de téléphone',
+                'attr' => [
+                    'placeholder' => 'Votre numéro de téléphone'
+                ]
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Ajouter mon adresse'
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn-block btn-info'
+                ]
             ]);
     }
 
